7a44e88a70b0cf0ad9a06cb28ab2aa5d2b4f5196
Kenny-alive <ksvcsv@inbox.ru>
2024-07-29 08:52:59 +0300

    init:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const absValue = Math.abs(n)
+  const nArr = absValue.toString().split('');
+  const result = [];
+  for (let i = nArr.length - 1; i >= 0; i--) {
+    result.push(nArr[i])
+  }
+  return result.join('');
 }
